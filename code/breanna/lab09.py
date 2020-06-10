f7352689944b450338207b48688d5d750ff3c7b3
Breanna Balcuns <bmbalcuns@live.com>
2020-06-10 09:44:27 -0700

    Version 1

--- code/breanna/lab09.py
+++ code/breanna/lab09.py
@@ -1,2 +1,13 @@
-# unit converter
+# Unit Converter
 
+def conversion():
+    num = input("Enter the distance in feet: ")
+    c_num = float(num) * 0.3048
+    print(f"{num} feet is {c_num} meters!")
+
+def main():
+    print("We are going to do some conversion!")
+    conversion()
+
+
+main()
