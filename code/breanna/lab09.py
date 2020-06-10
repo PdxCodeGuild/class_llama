12d783658a1a0fd9b509b480295f1933513d7ae1
Breanna Balcuns <bmbalcuns@live.com>
2020-06-10 11:13:59 -0700

    Version 4

--- code/breanna/lab09.py
+++ code/breanna/lab09.py
@@ -1,42 +1,63 @@
 # Unit Converter
 
-def conversion():
+def conversion_m():
     
     num = input("Enter the distance in numerical value: ")
-    unit = input("Enter the unit you would like to use [in, ft, yd, m, mi, km]: ")
+    input_unit = input("Enter the input unit [in, ft, yd, m, mi, km]: ")
+    output_unit = input("Enter the output unit [in, ft, yd, m, mi, km]: ")
 
-    if unit == "in":
-        in_num = float(num) * 0.0254
-        print(f"{num} in is {in_num} m!")
+    # convert to meters
+    if input_unit == "in":
+        num_m = float(num) * 0.0254
 
-    elif unit == "ft":
-        ft_num = float(num) * 0.3048
-        print(f"{num} ft is {ft_num} m!")
+    elif input_unit == "ft":
+        num_m = float(num) * 0.3048
 
-    elif unit == "yd":
-        yd_num = float(num) * 0.9144
-        print(f"{num} ft is {yd_num} m!")
+    elif input_unit == "yd":
+        num_m = float(num) * 0.914
+
+    elif input_unit == "m":
+        num_m = float(num) * 1
+
+    elif input_unit == "mi":
+        num_m = float(num) * 1609.34
+
+    elif input_unit == "km":
+        num_m = float(num) * 1000
+
+
+    # full conversion
+    if output_unit == "in":
+
+        print(f"{num} {input_unit} is {num_m/0.0254} {output_unit}!")
+
+    elif output_unit == "ft":
+
+        print(f"{num} {input_unit} is {num_m/0.3048} {output_unit}!")
+
+    elif output_unit == "yd":
+
+        print(f"{num} {input_unit} is {num_m/0.914} {output_unit}!")
     
-    elif unit == "m":
-        m_num = float(num) * 1
-        print(f"{num} m is {m_num} m!")
+    elif output_unit == "m":
 
-    elif unit == "mi":
-        mi_num = float(num) * 1609.34
-        print(f"{num} m is {mi_num} m!")
+        print(f"{num} {input_unit} is {num_m/1.0} {output_unit}!")
 
-    elif unit == "km":
-        km_num = float(num) * 1000
-        print(f"{num} m is {km_num} m!")
+    elif output_unit == "mi":
+
+        print(f"{num} {input_unit} is {num_m/1609.34} {output_unit}!")
+
+    elif output_unit == "km":
+
+        print(f"{num} {input_unit} is {num_m/1000.0} {output_unit}!")
 
     else:
         print("Sorry, that was not a valid input.")
 
 
-
 def main():
     print("We are going to do some conversion!")
-    conversion()
+    conversion_m()
 
 
 main()
