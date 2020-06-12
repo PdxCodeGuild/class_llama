b7d23950cd5d225c886ab4a1621a14d049bec1f0
Justin Chambers <justin36chambers@gmail.com>
2020-06-12 16:35:06 -0700

    updated

--- code/justin/lab14.py
+++ code/justin/lab14.py
@@ -2,58 +2,60 @@
 
 import random
 
-# Determines the winning numbers
+# Ticket number functions
 def pick6_winners():
     win = [random.randint(1, 99) for num in range(6)]
     return win
 
-# How much spent/won
-def balancer(times_run, winnings):
-    balance = 0
-    for x in times:
-        balance -= 2
-    if winnings > 0:
-        balance += int(winnings)
-    return balance
-    
-
-# Player chooses random 6
 def pick6_player():
     ticket = [random.randint(1, 99) for num in range(6)]
     return ticket
 
-
-# Compares indices
-def matches(winners, players):
+# Compares tickets
+def compared(winner, player):
     matches = 0
-    for players[i] in winners[i]:
-        matches += 1
+    for i in range(len(player)):
+        if player[i] == winner[i]:
+            matches += 1
     return matches
 
 # Returns prize from winning numbers
 def winnings(num):
     if num == 0:
-        return "nothing :("
+        return 0
     elif num == 1:
-        return "$4"
+        return 4
     elif num == 2:
-        return "$7"
+        return 7
     elif num == 3:
-        return "$100"
+        return 100
     elif num == 4:
-        return "$50,000"
+        return 50000
     elif num == 5:
-        return "$1,000,000"
+        return 1000000
     elif num == 6:
-        return "$25,000,000"
-
+        return 25000000
 
 def play_lottery():
+    balance = 0
     for x in range(100000):
-        pick6_winners()
-        pick6_player()
+        
+        balance -= 2
+        winner = pick6_winners()
+        chance = pick6_player()
+        matches = compared(winner, chance)
+        balance += winnings(matches)
 
 
+    print(f"Your final balance is ${balance}")
+
+    '''
+    if balance >= 0:
+        final_balance = balance + 200000
+        print(f"You broke even at least, impressive! You won {final_balance} altogether")
+    elif balance < 0 and balance > -200000:
+        print(f"Your  You won {final_balance} altogether")
+    '''
 
 
 play_lottery()
