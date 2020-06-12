1e9f83438e36ee06452f3c50080510d63d9d90fa
Jasmine Moore <jasmine.hope1988@gmail.com>
2020-06-12 13:38:05 -0700

    updated lab 19

---
+++ code/Jasmine/lcprac.py
@@ -0,0 +1,11 @@
+'''
+Problem 1
+Write a comprehension to generate the first 10 powers of two.
+
+[1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+
+''' 
+
+nums = [2**n for n in range(10)]
+[1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+print(nums)
