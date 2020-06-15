c07912e16f711f132f52ffdf8dd9e6a107052dc9
Dave <david.rice@technu.us>
2020-06-15 14:09:31 -0700

    initialize group project

---
+++ code/dave/mob_program/mob.py
@@ -0,0 +1,36 @@
+"""
+The goal is to calculate how many years it will take for two jackalopes to create a population of 1000.
+
+    Jackalopes are reproductive from ages 4-8 and die at age 10.
+    Gestation is instantaneous. Each gestation produces two offspring.
+    Jackalopes are hermaphrodites, it takes a pair to reproduce, but any pair will do
+
+With these conditions in mind, we can represent our population as a list of ints.
+"""
+
+# initialize age
+age = 0
+
+# initialize years
+year = 0
+
+# initialize jackalope count and list
+jackalope_count = 2
+jackalope = [0,0]
+
+
+# conditional statement to compare age to reproduction
+if age < 3 and > 8:
+    # does not produce offspring
+else:
+    # produces two offspring
+
+
+# jackalopes = [0,0]
+# jackalopes = [1,1] year + 1
+# jackalopes = [2,2] year + 2
+
+# jackalopes = [0,0,4,4] year + 4
+# jackalopes = [0,0,1,1,5,5]
+
+# length
