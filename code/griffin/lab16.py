faf6b92a89157df10fe077bb67ee71c4bba4f43d
unknown <impendingdelusion@gmail.com>
2020-06-17 12:24:36 -0700

    lab 16, version 2

--- code/griffin/lab16.py
+++ code/griffin/lab16.py
@@ -22,4 +22,35 @@ def greyscale():
     
     img.show()
 
-greyscale()
+
+
+def hsvify():
+    
+
+    for i in range(width):
+        for j in range(height):
+            r, g, b = pixels[i, j]        
+            
+            # colorsys uses colors in the range [0, 1]
+            h, s, v = colorsys.rgb_to_hsv(r/255, g/255, b/255)
+
+            #doing math at pixels
+            h = h/2
+            s = s * 2
+            v = v*1.3 
+                         
+            r, g, b = colorsys.hsv_to_rgb(h, s, v)        
+            
+            # convert back to [0, 255]                     
+            r = int(r*255)
+            g = int(g*255)
+            b = int(b*255)        
+            
+            pixels[i, j] = (r, g, b)
+        
+    img.show()
+
+    
+
+
+hsvify()
