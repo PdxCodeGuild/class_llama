d17a54780447cc5b5675e929f9d800d41b08ed65
Breanna Balcuns <bmbalcuns@live.com>
2020-06-17 16:27:55 -0700

    Version 1

---
+++ code/breanna/lab20.py
@@ -0,0 +1,16 @@
+# Credit Card Validation
+
+
+nums = 4, 5, 5, 6, 7, 3, 7, 5, 8, 6, 8, 9, 9, 8, 5, 5
+nums = list(nums)
+# print(nums)
+
+check_digit = nums[15]
+print(check_digit)
+ 
+del nums[15]
+print(nums)
+
+nums.reverse()
+print(nums)
+
