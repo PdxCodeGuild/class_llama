10df35079d3d02cfe8e061691e2b6f026a31c00f
unknown <impendingdelusion@gmail.com>
2020-06-18 10:13:17 -0700

    added a function to draw the bar graph. It kinda works

--- code/griffin/lab18.py
+++ code/griffin/lab18.py
@@ -16,9 +16,21 @@ def peaks_and_valleys(data):
     peaks(data)
     valleys(data)
 
+def draw_xs(data):
+    counter = 9
+    for i in range(9):
+        for num in data:
+            if num >= counter:
+                print("X  ", end = "")
+            else:
+                print("   ", end = "")
+        print("\n")
+        counter -= 1
 
 def main():
     data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+    draw_xs(data)
+    print(data)
     peaks_and_valleys(data)
     
 
