f7bc5d4867fc4109f1f9ffc7048256b2fbe694f3
unknown <impendingdelusion@gmail.com>
2020-06-18 09:52:17 -0700

    updating lab 18, version 1 complete

--- code/griffin/lab18.py
+++ code/griffin/lab18.py
@@ -5,18 +5,22 @@ def peaks(data):
                 print(f"peak at index {i}")
         
 
-def valleys():
-    pass
+def valleys(data):
+    for i, num in enumerate(data):
+        if i > 0 and i < len(data) -1:
+            if num < data[i-1] and num < data[i+1]:
+                print(f"valley at index {i}")
 
 
-def peaks_and_valleys():
-    pass
+def peaks_and_valleys(data):
+    peaks(data)
+    valleys(data)
 
 
 def main():
     data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+    peaks_and_valleys(data)
     
-    peaks(data)
 
 
 if __name__ == "__main__":
