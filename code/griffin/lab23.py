4a30e1e35900816ddc2570bf4d2ddfeb8e1b13a8
unknown <impendingdelusion@gmail.com>
2020-06-18 15:39:20 -0700

    adding lab 23, v2

--- code/griffin/lab23.py
+++ code/griffin/lab23.py
@@ -1,6 +1,5 @@
 with open('medieval contacts.csv', 'r') as file:
     lines = file.read().split('\n')
-    print(lines)
 
 dict_list = []
 
@@ -12,5 +11,31 @@ for line in lines:
         temp_dict[very_temp_list[0]] = very_temp_list[1]
     dict_list.append(temp_dict)
 
-print(dict_list)
+def create_contact():
+    medieval_myspace = {}
+    name = input("how doth thif one be known by thine companionf? ")
+    medieval_myspace["name"] = name
+    house = input("to which lord if their allegianfe pledged? ")
+    medieval_myspace["house"] = house
+    house_words = input("what cry fhalt they utter when charging upon enemief of their house? ")
+    medieval_myspace["house words"] = house_words
+    status = input("verily, how doth thif knave occupy their time? ")
+    medieval_myspace["status"] = status
+    dict_list.append(medieval_myspace)
+
+
+def main():
+    while True:
+        create_contact()
+        print(f"thine lift of ne'er-do-wellf if: {dict_list}")
+        stop_quest = input("if there another thou wifheft to record? (y/n) ")
+        if stop_quest == "n":
+            break
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
     
