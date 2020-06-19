da4f08bdf65a024ce00c73ce7971cf743d7986b7
Breanna Balcuns <bmbalcuns@live.com>
2020-06-19 11:54:32 -0700

    Version 1

--- code/breanna/lab22.py
+++ code/breanna/lab22.py
@@ -1,6 +1,8 @@
 # Automated Readability Index
 
 import math
+import re
+import string
 
 
 # get the book
@@ -9,11 +11,17 @@ with open("metamorphosis.txt", "r") as text:
 
 # find values for ari_score math
 characters = len(content)
-# print(characters)
-words = len(content.split())
-# print(words)
-sentences = len(content.split('.'))
-# print(sentences)
+print(characters)
+words = len(re.split(r"\s+", content))
+print(words)
+# below is the same thing without regex
+# words2 = len(content.split())
+# print(words2)
+sentences = len(re.split(r"[.?!]\s", content))
+print(sentences)
+# below only splits on "." and so has less sentences
+# sentences2 = len(content.split('.'))
+# print(sentences2)
 
 # get the decimal ari_num
 ari_score = math.ceil(4.71 * (characters/words) + 0.5 * (words/sentences) - 21.43)
