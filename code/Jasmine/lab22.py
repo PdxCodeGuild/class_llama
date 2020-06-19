dbd100710fa5de79f58dc1538500de0a8c55c111
Jasmine Moore <jasmine.hope1988@gmail.com>
2020-06-19 14:36:33 -0700

    updated lab 22

--- code/Jasmine/lab22.py
+++ code/Jasmine/lab22.py
@@ -32,22 +32,53 @@ words = 0
 
 with open(filename, 'r') as file:
     for line in file: 
-        wordsList = line.split()
+        wordsList = line.split() # returns list of word in str 
         lines += 1
         words += len(wordsList) # just get length of wordslist list
         chars += len(line) # gets length of line on each iteration of # of characters
 
-print("sentences:" , lines)
+print("lines:" , lines)
 print("characters:" , chars)
 print("words:" , words)
 
 # ARI formula 
 
-ARI = 4.71*(int(chars) / int(words)) + 0.5* (int(words) / int(lines)) - 21.43
+ari = 4.71*(int(chars) / int(words)) + 0.5* (int(words) / int(lines)) - 21.43
 
-print("The ARI is:" , round(ARI))
+print("The ARI of this material is:" , round(ari)) #rounds ARI 
+
+# use a dictionary to look up the age range and grade level equated with ARI
+# first step create dictionary 
+
+ari_scale = {
+     1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+     2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+     3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+     4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+     5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+     6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+     7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+     8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+     9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+    10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+    11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+    12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+    13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+    14: {'ages': '18-22', 'grade_level':      'College'}
+}
+
+#function should loop through the dict to find the grade level that correponds to the ARI score
+
+print(ari_scale[int(ari)]['ages'])
+
+    
 
 
+
+
+    
+
+     
 
 
 
