34b3b52500a3a5c53d73add9001508dc0525a0c9
unknown <impendingdelusion@gmail.com>
2020-06-24 14:35:29 -0700

    lab 25, version 2

--- code/griffin/lab25.py
+++ code/griffin/lab25.py
@@ -1,6 +1,9 @@
 class Account:
-    def __init__(self,balance=0):
+
+    def __init__(self,balance=0,transaction_record = []):
         self.balance = balance
+        self.transaction_record = transaction_record
+
 
     def check_balance(self):
         print(f'Your account contains ${self.balance}')
@@ -8,18 +11,24 @@ class Account:
     def deposit(self):
         dep = int(input("How much is deposited? "))
         self.balance+= dep
+        self.transaction_record.append(f"you deposited ${dep}")
 
     def withdraw(self):
         withdraw = int(input("How much would you like to withdraw? "))
         self.balance -= withdraw
+        self.transaction_record.append(f"you withdrew ${withdraw}")
 
     def check_withdrawal(self,amount):
         if self.balance >= amount:
             return True
         else:            
             return False
+
+    def print_transactions(self):
+        print(self.transaction_record)
             
 
 checking = Account(500)
-checking.check_withdrawal(600)
-checking.check_balance()
+checking.deposit()
+checking.withdraw()
+checking.print_transactions()
