e02c930daa7465179c89097e9fafcf84465b4bc2
Breanna Balcuns <bmbalcuns@live.com>
2020-06-24 15:57:11 -0700

    Version 1

---
+++ code/breanna/lab25.py
@@ -0,0 +1,28 @@
+# ATM
+
+
+class ATM:
+    
+    def __init__(self):
+        self.balance = 0
+        
+    def check_balance(self):
+        print(self.balance)
+
+    def deposit(self, amount):
+        self.balance += deposit_amount
+
+    def check_withdrawal(self, amount):
+        if self.balance - check_amount <= 0:
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount):
+        if self.check_withdrawal() == True:
+            self.balance -+ withdraw_amount
+            print(f"Your new balance is ${self.balance}.")
+        else:
+            print("You cannot withdraw that amount.")
+
+
