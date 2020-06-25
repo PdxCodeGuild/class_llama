4323840457a9e79e282f05256ae08109c6fadb31
Breanna Balcuns <bmbalcuns@live.com>
2020-06-25 15:03:39 -0700

    Version 1

--- code/breanna/lab26.py
+++ code/breanna/lab26.py
@@ -1,13 +1,13 @@
 # Tic-Tac-Toe
 
 
-# class Player:
+class Player:
     
-    # def __init__(self):
-        # self.name1 = player1
-        # self.name2 = player2
-        # self.token1 = 'X'
-        # self.token2 = 'O'
+    def __init__(self):
+        self.name1 = player1
+        self.name2 = player2
+        self.token1 = 'X'
+        self.token2 = 'O'
 
 
 class Game:
@@ -26,37 +26,39 @@ class Game:
             '2,2': ' '
         }
 
-        count = 0
+        self.count = 0
 
     def __repr__(self):
         print(f"{self.board['0,0']}|{self.board['1,0']}|{self.board['2,0']}\n- - -\n{self.board['0,1']}|{self.board['1,1']}|{self.board['2,1']}\n- - -\n{self.board['0,2']}|{self.board['1,2']}|{self.board['2,2']}")
 
 
-#     def move(self, x, y, player): 
-#         # place a player's token character string at a given coordinate
-#         # (top-left is 0, 0), x is horizontal position, y is vertical position
-#         play.move(2, 1, player_1)
-            # count += 1
+    def move(self, x, y, player):
+        play.move(2, 1, player1)
+        self.count += 1
 
-#     def cal_winner(self):
-#         # what token character string has won or None if no one has
-#         play.calc_winner()
+    def winner(self):
+        # if statement?
 
-#     def is_full(self):
-#         # returns True if the game board is full
-#         play.is_full()
-        # print("The board is full. End of game.") 
+    def is_full(self):
+        if self.count == 9:
+            print("The board is full. End of game.") 
+        elif self.count != 9:
+            pass
 
-#     def is_game_over(self):
-#         # returns True if the game board is full or a player has won
-#         play.is_game_over()
+    def is_game_over(self):
+        self.is_full()
 
-# player = Player()
 play = Game()
 
 def main():
     print("Let's play Tic-Tac-Toe!")
     play.__repr__()
+    
+    player1 = input("Enter the first player's name: ")
+    print(f"{player1}, you are 'X'.")
+    player2 = input("Enter the second player's name: ")
+    print(f"{player2}, you are 'O'.")
+
 
 
 main()
