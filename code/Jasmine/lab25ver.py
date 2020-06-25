5c12c16c75a60acf1130b0a46ccae04a249554cf
Jasmine Moore <jasmine.hope1988@gmail.com>
2020-06-25 10:59:38 -0700

    another version of lab 25

---
+++ code/Jasmine/lab25ver.py
@@ -0,0 +1,74 @@
+'''
+Let's represent an ATM with a class containing a balance property. A newly created account
+ will default to a balance of 0. Implement the initializer, as well as the following methods:
+
+check_balance() returns the account balance
+deposit(amount) deposits the given amount in the account
+check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the negative
+withdraw(amount) withdraws the amount from the account and returns it
+
+'''
+class bankaccount: 
+    def __init__(self, balance=0):
+        self.balance = balance
+        self.transactions = []
+    
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        self.transactions.append(f'you deposited {amount}')
+        return self.balance
+        
+    def checkwithdrawl(self, amount): 
+        if self.balance >= amount:
+            return True
+        return False
+
+    def withdrawl(self, amount):
+        if self.checkwithdrawl(amount): #checks the amount is good for withdrawl
+            self.balance = self.balance - amount
+            self.transactions.append(f'you withdrew {amount}')
+        else: 
+            print("insufficent funds")
+        return self.balance
+
+    def checkbalance(self):
+        return self.balance
+
+    def print_transactions(self):
+        for amount in self.transactions: 
+            print(amount)
+    def display(self):
+        print("Net Available Balance:" , self.balance)
+
+account = bankaccount()
+# account.deposit(int(input("how much would like to deposit: ")))
+# account.withdrawl(int(input("how much would like to withdraw: ")))
+# print(account.checkbalance())
+# account.print_transactions()
+
+#def atm():
+while True:
+
+    user = input("Welcome to the ATM! Select an option: deposit, withdraw, check balance, history, other: ")
+    if user == "deposit": 
+        useramount = int(input("how much would you like to deposit: "))
+        account.deposit(useramount)
+        #print()
+    elif user == "withdraw": 
+        userwithdraw = int(input("how much would you like to withdraw: "))
+        account.withdrawl(userwithdraw)
+    elif user == "check balance": 
+        account.checkbalance()
+    elif user == "history":
+        account.print_transactions()
+    # elif user == "done":
+    #     break
+    elif user == "other": 
+        user1 = input("would you like another transaction: ")
+        if user1 == "yes":
+            user1 == user
+        elif user1 == "no":
+            break
+
+
+#atm()
