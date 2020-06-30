128c801e6224d4e7e7a3ffb7f5a24e62539a5cc3
Eric Romero <estur76@gmail.com>
2020-06-30 16:30:39 -0700

    Updates for Mini_Capstone idea

--- code/Eric/Mini_Capstone.py
+++ code/Eric/Mini_Capstone.py
@@ -1,4 +1,5 @@
-# Idea for Mini Capstone
+# Idea for Mini Capstone: ****U/Record**** (possible name for the web app)
+
 
 # My mini capstone would be a start to help struggling artists with a free, simple, and easy to use
 # recording online web app! Record original music or voice over work. All the features needed to 
@@ -6,12 +7,13 @@
 # YouTube Artist Channel and upload your recordings! As well as eliminate record labels and all the hassles
 # with "fine print" in record contracts! Create an album, go to 'https://cdbaby.com/' and they distribute to 
 # a lot of websites (iTunes, Amazon Prime, Google Music, YouTube Music and Spotify) where people can purchase 
-# your album or songs. 
+# your album or songs.
 
 """
 Might lead into the final Capstone Project
 Potential to add music theory (notation)
 Metronome or Back Beat to play along with...
+Potential for partnerships with existing entities in Music and Entertainment Industry!
 """
 
 import pyaudio
