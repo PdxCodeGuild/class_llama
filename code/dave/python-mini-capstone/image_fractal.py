834501f230a836e9250958bfeeb22b850926191a
dave <david.rice@technu.us>
2020-07-01 12:15:30 -0700

    images added to img, and consolidated all snippets into image_fractal.py

--- code/dave/python-mini-capstone/image_fractal.py
+++ code/dave/python-mini-capstone/image_fractal.py
@@ -105,6 +105,20 @@ print(img_new_rz.shape)
 #----------------------------------------------------------------------------#
 
 # import modules
+from PIL import Image
 
+# create blank background
+img = Image.new("RGB", (1200,1080), color="green")
+# img.show() # TEST: pass - green background displayed 400x400
 
+# save as 'green_bg
+img.save("./img/green_bg.png")
+# img.show() # TEST: pass - green_bg saved to location
 
+# define x,y and offset 
+x,y = img.size
+offset = x // 10, y // 7
+
+# paste image onto 
+img.paste(Image.open("./img/Gnome_G017_HD_NoLogo.png").resize((200,200)), offset)
+img.show()
