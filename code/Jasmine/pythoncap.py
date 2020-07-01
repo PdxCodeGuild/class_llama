1c39ba1db665cb4f8f1fbdbb2c2a7676017c978a
Jasmine Moore <jasmine.hope1988@gmail.com>
2020-07-01 12:30:15 -0700

    updates

--- code/Jasmine/pythoncap.py
+++ code/Jasmine/pythoncap.py
@@ -14,18 +14,26 @@ with open('IMG_7280.jpg' , 'rb') as image_file:
     my_image.has_exif
     True
 
-dir(my_image)
+dir(my_image) #lists out all the 
 print(dir(my_image))
 
-for tag in dir(my_image):  
-    if tag not in ['_exif_ifd_pointer' , '_segments' , 'custom_rendered' , 'compression' , 'get' , 'get_file' , 'get_thumbnail' , 'delete' , 'delete_all' , 'datetime_digitized' , 'datetime_original' , 'exif_version' , 'has_exif' , 'offset_time' , 'rescommended_exposure_index' , 'resolution_unit' , 'scene_capture_type' , 'lens_specification' , 'metering_mode' , 'subsec_time_digitized' , 'subsec_time_original']:
-        print(tag , my_image[tag])
 
-image_stats_dict = []
-
+#convert list of image stats to create a keys:values dict (k eys would dir() and tags would be values)
+# dir() == keys,  my_image[tag] == values
 def convert(): 
-    
+    tag_list = []
+    for tag in dir(my_image):  #returns all the metadata of the image
+        if tag not in ['_exif_ifd_pointer' , '_segments' , 'custom_rendered' , 'compression' , 'get' , 'get_file' , 'get_thumbnail' , 'delete' , 'delete_all' , 'datetime_digitized' , 'datetime_original' , 'exif_version' , 'has_exif' , 'offset_time' , 'rescommended_exposure_index' , 'resolution_unit' , 'scene_capture_type' , 'lens_specification' , 'metering_mode' , 'subsec_time_digitized' , 'subsec_time_original']: #
+            tag_list.append(tag)
+    imagemeta_dict = {}
+    for tag in tag_list: 
+        imagemeta_dict[tag] = my_image[tag]
+    print(imagemeta_dict)
 
+
+convert()
+
+metadata = []
 
 
 
