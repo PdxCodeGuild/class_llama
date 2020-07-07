0912f7f4c791164baf4ca09b7b79a03d1838ca8b
Justin Chambers <justin36chambers@gmail.com>
2020-07-07 09:44:40 -0700

    updated

--- code/justin/Python/minicapstone.py
+++ code/justin/Python/minicapstone.py
@@ -1,4 +1,5 @@
 import os
+
 #from tkinter import Tk
 #from tkinter.filedialog import askdirectory
 #path = askdirectory()
@@ -6,9 +7,9 @@ import os
 os.chdir('../Desktop/First Test')
 path = os.walk('.')
 
-def audio_lister(path):
+def audio_lister(start_path = '.'):
     audios = []
-    for root, dirs, files in path:
+    for root, dirs, files in start_path:
         for f in files:
             if f.endswith(('.wav', '.aif')):
                 audios.append(f)
@@ -23,23 +24,28 @@ def get_size(start_path = '.'):
                 total_size += os.path.getsize(fp)
     return total_size
 
-def average_size(size):
-    total_size = 0
-    for root, dirs, files in os.walk(start_path):
-        for f in files:
-            fp = os.path.join(root, f)
-            if not os.path.islink(fp):
-                total_size += os.path.getsize(fp)
-    return total_size
+def average_size(files, size, units):
+    num_files = int(len(files))
+    if units == 'gb':
+        converted_size = size / 1000000000
+    elif units == 'mb':
+        converted_size = size / 1000000
+    average = (int(converted_size) / num_files)
+    print(f'The average size per file is {int(average)} {units}')
 
 audio_files = audio_lister(path)
-print(audio_files)
-print(get_size())
-
-# size of given directory
 num_bytes = get_size()
-num_mb = (num_bytes / 1000000)
 
+def convert_units(num, units):
+    if units == 'gb':
+        print(f'This folder contains {int(num / 1000000000)} GB of audio files.')
+    elif units == 'mb':
+        print(f'This folder contains {int(num / 1000000)} MB of audio files.')
+    
+
+units = input('Units in MB or GB?: ').lower()
+convert_units(num_bytes, units)
+average_size(audio_files, num_bytes, units)
 
 
 
