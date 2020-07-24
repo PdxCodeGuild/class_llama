35b463a9726a7eeaad9c3d39a92b22a5ebc7b1fd
Jasmine Moore <jasmine.hope1988@gmail.com>
2020-07-24 09:44:50 -0700

    updates

--- code/Jasmine/DJANGO_LAB_1/grocery_list/views.py
+++ code/Jasmine/DJANGO_LAB_1/grocery_list/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.utils import timezone
 
 from .models import GroceryItem
 
-def Index(request): 
-    return render(request, 'grocery_list/index.html')
+def index(request): 
+    grocery_item = GroceryItem.objects.all()
+    context = {
+        'grocery_item': grocery_item
+    }
+    print(context)
+    return render(request, "grocery_list/index.html", context)
 
-def createitem(request):
+ 
+def itemlist(request):
     post_item = request.POST['grocery']
-    GroceryItem.objects.create(item=post_item, created_date=timezone.now())
+    GroceryItem.objects.create(item=post_item, create_date=timezone.now())
+    return HttpResponseRedirect(reverse('grocery_list:index'))
 
+def update(request, grocery_id): 
+    complete = get_object_or_404(GroceryItem, pk= grocery_id)
+    complete.complete_date = timezone.now()
+    complete.is_done = True
+    complete.save()
+    return HttpResponseRedirect(reverse('grocery_list:index'))
+
+def delete(request, grocery_id):
+    deleteitem = get_object_or_404(GroceryItem, pk= grocery_id)
+    
