bb5b3d55ae75bae5868c098c91eba339ea892948
mr-marcus-ivey <mr.marcuslivey@gmail.com>
2020-07-24 15:26:20 -0700

    'upadted'

---
+++ code/marcus/grocerylab/grocerlist/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GrocerlistConfig(AppConfig):
+    name = 'grocerlist'
---
+++ code/marcus/grocerylab/grocerlist/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import Grocery_List
+
+class ListForm(forms.ModelForm):
+
+    class Meta:
+        model = Grocery_List
+        fields = ('Item', '',)
