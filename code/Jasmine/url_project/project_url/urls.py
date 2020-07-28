9b094de5c255410a2e1a4f082ccc1648cef9f0fd
Jasmine Moore <jasmine.hope1988@gmail.com>
2020-07-28 12:15:55 -0700

    updates

--- code/Jasmine/url_project/project_url/urls.py
+++ code/Jasmine/url_project/project_url/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
+app_name = 'url_project'
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
--- code/Jasmine/url_project/url_project/views.py
+++ code/Jasmine/url_project/url_project/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+import string
 
 from .models import url
 
 def index(request): 
+    return render(request, 'form/index.html')
 
-    
-    return 
+
+def get_convert(request): 
+
+
+def redirect(request):
+  get_url = url.objects.all()
+    context = { 
+        'get_url': url
+    }
+    return
