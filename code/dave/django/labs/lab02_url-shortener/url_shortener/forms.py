f9b86d574e87124f6b070810d5fdbd60213192d5
dave <david.rice@technu.us>
2020-07-28 15:47:39 -0700

    completed view for submit

--- code/dave/django/labs/lab02_url-shortener/url_shortener/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import URLShortener
-
-class UrlShortenerForm(ModelForm):
-    class Meta:
-        model = URLShortener
-        fields = ['long_url']
