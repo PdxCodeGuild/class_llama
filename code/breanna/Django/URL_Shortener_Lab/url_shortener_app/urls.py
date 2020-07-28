b289db27a17815fe9e1643dd23fd05c5debf71d3
bmbalcuns <66447278+bmbalcuns@users.noreply.github.com>
2020-07-28 15:48:06 -0700

    URL Shortener Lab

--- code/breanna/Django/URL_Shortener_Lab/url_shortener_app/urls.py
+++ code/breanna/Django/URL_Shortener_Lab/url_shortener_app/urls.py
@@ -5,5 +5,5 @@ app_name = 'url_shortener'
 urlpatterns = [
     path('', views.index, name='index'),
     path('submit/', views.submit_url, name='submit'),
-    path('<str:short_url>/', views.redirect, name='redirect')
+    path('<str:short_url>/', views.redirect, name='redirect') 
 ]
