5e56d3ee846bf2cc39528693ae0fb4a7deecd5c9
Breanna Balcuns <bmbalcuns@live.com>
2020-07-29 12:22:46 -0700

    URL Shortener Lab

--- code/breanna/Django/URL_Shortener_Lab/url_shortener_app/models.py
+++ code/breanna/Django/URL_Shortener_Lab/url_shortener_app/models.py
@@ -4,6 +4,7 @@ class UrlShortener(models.Model):
 
     long_url = models.CharField(max_length=100)
     short_url = models.CharField(max_length=6)
+    clicks = models.IntegerField(default=0)
 
     def __str__(self):
         return self.long_url 
