2b0c04c8e1bda5d000921c5be10e277d10669cb5
dave <david.rice@technu.us>
2020-07-30 09:59:29 -0700

    updated lab02

--- code/dave/django/labs/lab02_url-shortener/shorturl/models.py
+++ code/dave/django/labs/lab02_url-shortener/shorturl/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class ShortUrl(models.Model):
-    long_url = models.URLField('URL')
-    short_code = models.CharField(max_length=10, blank=True)
 
+    long_url = models.URLField('URL', unique=True)
+    short_code = models.CharField(max_length=10, blank=True)
+    
     def __str__(self):
         return str(self.long_url)
