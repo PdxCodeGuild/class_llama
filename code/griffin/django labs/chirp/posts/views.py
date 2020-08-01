45c11539e75687f0dce843b53f2ac4915c1123b8
Griffin Wimminger <impendingdelusion@gmail.com>
2020-07-31 18:33:11 -0700

    L

--- code/griffin/django labs/chirp/posts/views.py
+++ code/griffin/django labs/chirp/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+class ChirpListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+# class ChirpCreateView(CreateView):
+#     model = Post
+#     template_name = post_chirp.html
+#     fields = ['chirp']
