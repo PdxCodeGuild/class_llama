3a7536f1961146ba188696f64c7d0529b182b5f4
mr-marcus-ivey <mr.marcuslivey@gmail.com>
2020-08-03 14:39:25 -0700

    'Updating'

--- code/marcus/chirplab/users/views.py
+++ code/marcus/chirplab/users/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
 
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
 
+class UserProfileView(DetailView):
+    model = User
+    template_name = 'user_profile.html'
 
-
-
-
-
-
-
-
-
-
+    def get_object(self):
+        return get_object_or_404(User, username=self.kwargs['username'])
