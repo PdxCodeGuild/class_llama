5b8261a1db397e4f398e575ca99ef2a73e467674
dave <david.rice@technu.us>
2020-08-05 16:34:53 -0700

    lab02 of pick_3 labs complete and working

--- code/dave/javascript/labs/lab01-03-pick_3/lab02-pick_3.js
+++ code/dave/javascript/labs/lab01-03-pick_3/lab02-pick_3.js
@@ -2,50 +2,49 @@
 This program is an ATM that allows: checks balance, deposit, check withdraw(amount) and withdraws(amount)
 */
 
-// create the ATM class
-let atm = {
 
-    // initialization function
-    init: function(this, balance){
-        return this.balance = balance;
-    },
+// create the ATM class
+class Atm {
+
+    constructor() {
+        this.balance = 0;
+        return this.balance;
+    }
 
     // Declare multiple functions for each actions
     
     // func that checks balance
-    check_balance: function(this, balance){
+    check_balance() {
         return this.balance;
-    },
+    }
 
     // func that adds amount to balance
-    deposit: function(this, amount){
+    deposit(amount) {
         this.balance += amount;
         return this.balance;
-    },
+    }
         
     // func that checks account balance
-    check_withdrawl: function(this, amount){
+    check_withdrawl(amount) {
         if (this.balance >= amount) {
             return console.log("AVAILABLE");
         }
         else {
             return console.log("NOT AVAILABLE");
         }
-    },
-        
-
+    }
 
     // func that withdraws the amount requested and returns it
-    withdraw: function(this, amount){
+    withdraw(amount) {
         this.balance -= amount;
         return this.balance;
-    }   
-};
+    }
+}   
 
+let customer1;
+customer1 = new Atm();
 
-// let atm = console.log(Atm());
-// console.log("Your balance is: ${atm.check_balance(0)}");
-// console.log("Deposited amount: ${atm.deposit(60)}");
-// console.log("The funds you requested are {atm.check_withdrawl(60)}");
-// console.log("Your account balance is: ${atm.withdraw(20)}");
-console.log("Your balance is: " + atm.check_balance());
+console.log(customer1.check_balance());
+console.log(customer1.deposit(5));
+console.log(customer1.check_balance());
+
