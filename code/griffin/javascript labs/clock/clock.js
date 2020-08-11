e74ce2811160772d3e75e91460f1224a74dbc018
Griffin Wimminger <impendingdelusion@gmail.com>
2020-08-11 15:07:40 -0700

    weird bug

--- code/griffin/javascript labs/clock/clock.js
+++ code/griffin/javascript labs/clock/clock.js
@@ -1,11 +1,15 @@
 let date = new Date();
+let stopdate = new Date();
 let hour = date.getHours();
 let minutes = date.getMinutes();
 let seconds = date.getSeconds();
+let stopseconds = 0;
 
 let time_display = document.getElementById("time");
+let startstop = document.getElementById("startstop");
+let stopwatch = document.getElementById("stopwatch");
 
-function update() {
+function update_time() {
     date = new Date();
     hour = date.getHours();
     minutes = date.getMinutes();
@@ -14,4 +18,18 @@ function update() {
     time_display.innerText = time;
 }
 
-window.setInterval(update,1000);
+window.setInterval(update_time,1000);
+
+function update_stopwatch() {
+    stopseconds += 1;
+    stopdate.setSeconds(stopseconds);
+    
+    stopwatch.innerText = `${stopdate.getMinutes()}:${stopdate.getSeconds()}`;
+}
+
+startstop.addEventListener("click", function() {
+    stopdate = new Date();
+    stopdate.setHours(0,0,0,0);
+    window.setInterval(update_stopwatch,1000);
+
+})
