d6551970d35d7f8a550f88a3ee655472ab9c013e
dave <david.rice@technu.us>
2020-08-11 16:31:54 -0700

    NaN error with lab01-ver2

--- code/dave/javascript/labs/lab01-03-pick_3/lab01b/lab01-ver2.js
+++ code/dave/javascript/labs/lab01-03-pick_3/lab01b/lab01-ver2.js
@@ -8,60 +8,42 @@ var card_values = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 
 
 
 // initailize variables
-let card_sum = 0
+let card_sum = 0;
 
-function main(){
+let first_card = document.getElementById("num1").value;
+let second_card = document.getElementById("num2").value;
+let third_card = document.getElementById('num3').value;
 
-    
+let calculate = document.getElementById("calculate");
 
-//     /*  
-//     prompt user for 3 cards create while loop that asks
-//     for user card prompt. if not valid it will keep
-//     prompting until correct value is prompt
-//     */
-//     let first_card = prompt("What's your first card? ").toUpperCase();
-//     while (!(first_card in card_values)) {
-//         first_card = prompt("Not a valid card, enter another card value for first card: ").toUpperCase();
-//     }
-//     console.log(first_card);
+function advise(){
+    calculate.addEventListener("click", function() {
+        
 
-
-//     let second_card = prompt("What's your second card? ").toUpperCase();
-//     while (!(second_card in card_values)){
-//         second_card = prompt("Not a valid card, enter another card value for second card: ").toUpperCase();
-//     }
-//     console.log(second_card)
+        first_card = card_values[first_card];
+        second_card = card_values[second_card];
+        third_card = card_values[third_card];
 
 
-//     let third_card = prompt("What's your third card? ").toUpperCase();
-//     while (!(third_card in card_values)){
-//         third_card = prompt("Not a valid card, enter another card value for third card: ").toUpperCase();
-//     }
-//     console.log(third_card);
+        let card_sum = first_card + second_card + third_card;
 
-    let first_card = document.getElementById("num1").value;
-    let second_card = document.getElementById("num2").value;
-    let third_card = document.getElementById('num3').value;
-
-    first_card = card_values[first_card];
-    second_card = card_values[second_card];
-    third_card = card_values[third_card];
-
-
-    let card_sum = first_card + second_card + third_card;
-
-    if (card_sum < 17) {
-        document.getElementById('advice').innerHTML = 'Hit';
-    }
-    else if (card_sum > 17 && card_sum < 21) {
-        document.getElementById('advice').innerHTML = 'Stay';
-    }
-    else if (card_sum == 21) {
-        document.getElementById('advice').innerHTML = 'Blackjack!!';
-    }    
-    else {
-        document.getElementById('advice').innerHTML = 'Already Busted';
-    }
+        if (card_sum < 17) {
+            document.getElementById('advice').innerHTML = 'Hit';
+            console.log((card_sum) + " Hit");
+        }
+        else if (card_sum > 17 && card_sum < 21) {
+            document.getElementById('advice').innerHTML = 'Stay';
+            console.log((card_sum) + " Stay");
+        }
+        else if (card_sum == 21) {
+            document.getElementById('advice').innerHTML = 'Blackjack!!';
+            console.log((card_sum) + " Blackjack!!");
+        }    
+        else {
+            document.getElementById('advice').innerHTML = 'Already Busted';
+            console.log((card_sum) + " Already Busted");
+        }
+    });
 }
 
-console.log(main());
+advise();
