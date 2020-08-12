9833b95e8d730fe89439599f91c611a3a2d429b6
dave <david.rice@technu.us>
2020-08-12 10:18:09 -0700

    changed js in ver2

--- code/dave/javascript/labs/lab01-03-pick_3/lab01b/lab01-ver2.js
+++ code/dave/javascript/labs/lab01-03-pick_3/lab01b/lab01-ver2.js
@@ -10,40 +10,34 @@ var card_values = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 
 // initailize variables
 let card_sum = 0;
 
-let first_card = document.getElementById("num1").value;
-let second_card = document.getElementById("num2").value;
-let third_card = document.getElementById('num3').value;
+let first_card = document.getElementById("num1");
+let second_card = document.getElementById("num2");
+let third_card = document.getElementById('num3');
 
 let calculate = document.getElementById("calculate");
 
-function advise(){
-    calculate.addEventListener("click", function() {
-        
+calculate.addEventListener("click", function() {
+    first_card = card_values[first_card.value];
+    second_card = card_values[second_card.value];
+    third_card = card_values[third_card.value];
 
-        first_card = card_values[first_card];
-        second_card = card_values[second_card];
-        third_card = card_values[third_card];
+    let card_sum = first_card + second_card + third_card;
 
+    if (card_sum < 17) {
+        document.getElementById('advice').innerHTML = 'Hit';
+        console.log((card_sum) + " Hit");
+    }
+    else if (card_sum > 17 && card_sum < 21) {
+        document.getElementById('advice').innerHTML = 'Stay';
+        console.log((card_sum) + " Stay");
+    }
+    else if (card_sum == 21) {
+        document.getElementById('advice').innerHTML = 'Blackjack!!';
+        console.log((card_sum) + " Blackjack!!");
+    }    
+    else {
+        document.getElementById('advice').innerHTML = 'Already Busted';
+        console.log((card_sum) + " Already Busted");
+    }
+});
 
-        let card_sum = first_card + second_card + third_card;
-
-        if (card_sum < 17) {
-            document.getElementById('advice').innerHTML = 'Hit';
-            console.log((card_sum) + " Hit");
-        }
-        else if (card_sum > 17 && card_sum < 21) {
-            document.getElementById('advice').innerHTML = 'Stay';
-            console.log((card_sum) + " Stay");
-        }
-        else if (card_sum == 21) {
-            document.getElementById('advice').innerHTML = 'Blackjack!!';
-            console.log((card_sum) + " Blackjack!!");
-        }    
-        else {
-            document.getElementById('advice').innerHTML = 'Already Busted';
-            console.log((card_sum) + " Already Busted");
-        }
-    });
-}
-
-advise();
