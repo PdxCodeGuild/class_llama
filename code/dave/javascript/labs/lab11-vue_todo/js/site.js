4367d6466ea37a4c6f4924147c9d8da4444867a6
dave <david.rice@technu.us>
2020-08-25 13:49:57 -0700

    mvp vue todo list

--- code/dave/javascript/labs/lab11-vue_todo/js/site.js
+++ code/dave/javascript/labs/lab11-vue_todo/js/site.js
@@ -1,6 +1,17 @@
-new Vue({
+const app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello world!'
+        title: 'ToDoApp',
+        newTodo: '',
+        todos: []
+    },
+    methods: {
+        addTodo() {
+            this.todos.push({
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo = '';
+        }
     }
-})
+});
