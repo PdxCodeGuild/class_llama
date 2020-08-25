f98d709322f33de51154d9d2f00e052233d9ecfd
Breanna Balcuns <bmbalcuns@live.com>
2020-08-25 14:26:27 -0700

    DRF Example Lab

--- code/breanna/Django/DRF-Example_Lab/students_project/settings.py
+++ code/breanna/Django/DRF-Example_Lab/students_project/settings.py
@@ -38,8 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students.apps.StudentsConfig',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'rest_framework',
+    'api.apps.ApiConfig'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
